b5815bc45786eab1666c428f2871fb9b3de651ba
Amil44 <amil44@github.com>
2026-06-29 17:27:12 +0400

    Butun qovluqlar ve proyektler tam sekilde yuklendi

--- SOLUTION15/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using TaskLinq.DEbtorss;
-List<Debtor> debtors = Debtor.debtors;
-
-
-Console.WriteLine("======= SOLUTION15 =======");
-var sol15 = debtors.Sum(x => x.Debt) / debtors.Count();
-Console.WriteLine(sol15);
